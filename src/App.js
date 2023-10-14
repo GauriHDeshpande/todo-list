456c108cc320344041b0f379c6e2cd87b82118d7
unknown <gauri.deshpande22@rediffmail.com>
2023-10-14 20:14:10 +0530

    Added components in the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './MyComponents/Footer';
+import Header from './MyComponents/Header';
+import Todos from './MyComponents/Todos';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Gauri Deshpande
-          
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="My-Todo's-List"/>
+      <Footer/>
+      <Todos/>
+    </>
   );
 }
 
---
+++ src/MyComponents/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Todo() {
+  return (
+    <>
+      <h1>Todo works!</h1>
+    </>
+  );
+}
