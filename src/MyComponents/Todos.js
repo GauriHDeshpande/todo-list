0aa46530e6dd6f784157ef59a639ce9c6fbf27ae
unknown <gauri.deshpande22@rediffmail.com>
2023-10-26 13:03:37 +0530

    added horizontal lines between todos

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -3,17 +3,21 @@ import TodoItem from './TodoItem';
 
 export default function Todos(props) {
   const myStyle = {
-    minHeight : "100vh",
+    minHeight: "100vh",
     margin: "50px auto"
   }
   return (
-    <>
       <div className='container my-3' style={myStyle}>
         <h3 className='my-3'>Todos List</h3>
-        {props.todos.length === 0 ? "No todos to display" : props.todos.map((todo) => {
-          return <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
-        })}
+        {props.todos.length === 0 ? "No todos to display" :
+          props.todos.map((todo) => {
+            return (
+              <>
+                <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete} /><hr />
+              </>
+            )
+          })
+        }
       </div>
-    </>
   );
 }
